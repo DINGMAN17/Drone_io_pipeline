efd646566fa22eb2d2eba2cf36cbdbda9f4a44bc
DING MAN <55436197+DINGMAN17@users.noreply.github.com>
2021-01-09 18:57:47 +0800

    change writing absolute file path

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ Created on Sat Jan  9 16:21:46 2021
 """
 import os
 
-def create_image_list(folder_path, file_type='.png'):
+def create_image_list(folder_path=None, file_type='.png'):
     '''
     create a txt file which contains the absolute path of all test images.
     the txt file will be fed into semantic/defect segmentation model.
@@ -17,12 +17,14 @@ def create_image_list(folder_path, file_type='.png'):
     file_type : type of image file
                 The default is '.jpg'
     '''   
-    #path = "C:/Users/cryst/Study/Thesis/images" #for testing
-    path = folder_path 
+    path = "C:/Users/cryst/Study/Thesis/images" #for testing
+    #path = folder_path 
     file_type = file_type            
         
     with open('ss_test_img_list.txt', 'w') as file:
-        file.writelines([os.path.abspath(f)+'\n' for f in os.listdir(path) if f.endswith(file_type)])
+        file.writelines([path + '/' + f +'\n' for f in os.listdir(path) if f.endswith(file_type)])
 
     return os.path.abspath('ss_test_img_list.txt')
+
+create_image_list()
 
