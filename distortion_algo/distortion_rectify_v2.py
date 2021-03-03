5ce7342674ecac95aed704725912e8d5fe58a000
DING MAN <55436197+DINGMAN17@users.noreply.github.com>
2021-03-03 11:43:18 +0800

    Updated XT2 camera parameters

--- distortion_algo/distortion_rectify_v2.py
+++ distortion_algo/distortion_rectify_v2.py
@@ -3,7 +3,6 @@
 # =============================================================================
 # reference web site
 # https://docs.opencv.org/master/dc/dbb/tutorial_py_calibration.html
-# (japanese)　https://www.qoosky.io/techs/67a3d876c4
 # =============================================================================
 
 # =============================================================================
@@ -11,62 +10,67 @@
 # Outline: rectify original images to undistorted images 
 # =============================================================================
 
-# =============================================================================
-#           # input
-# calibration_parameters: calibration_parameters.npy
-# original images to undistortion: folder '01_org_img'
-#           # output
-# output undistorted images: folder '02_undis_img'
-# output undistorted images: folder '03_undis_img_roi'
-# =============================================================================
-
 import numpy as np
 import cv2 as cv
 import glob
 import os
 
-# load parameters
-parameter_arry = np.load('calibration_parameters.npy', allow_pickle=True)
-err, KK, distCoeffs, rvecs, tvecs = parameter_arry[:]
+def correct_distortion(input_path, rgb=True, output_path=None):
+    # create output path
+    if output_path is not None:
+        if not os.path.exists(output_path):
+            os.makedirs(output_path)
+    
+    # load parameters
+    if rgb:
+        parameter_arry = np.load('calibration_parameters_rgb.npy', allow_pickle=True)
+        prefix = '*.jpg'
 
-## check calibaration parameters
-# print('err', err)
-# print('KK', KK)
-# print('distCoeffs', distCoeffs)
-# print('rvecs', rvecs)
-# print('tvecs', tvecs)
+    else:
+        parameter_arry = np.load('calibration_parameters_ir.npy', allow_pickle=True)
+        prefix = '*.JPG'
+    
+    err, KK, distCoeffs, rvecs, tvecs = parameter_arry[:]
 
-# read images
-images = glob.glob('01_org_img/*.jpg')
-for fname in images: 
-    targetImage = cv.imread(fname)
-    
-    # camera array
-    targetImageSize = targetImage.shape[:2][::-1]
-    alpha = 1  # 1:black
-    newKK, roiSize = cv.getOptimalNewCameraMatrix(KK, distCoeffs, targetImageSize, alpha, targetImageSize)
-    
-    # distotion correct map
-    mapX, mapY = cv.initUndistortRectifyMap(KK, distCoeffs, None, newKK, targetImageSize, cv.CV_32FC1)
-    
-    # distortion
-    undistortedImage1 = cv.remap(targetImage, mapX, mapY, cv.INTER_LINEAR)
-    
-    # remove black parts
-    x, y, w, h = roiSize
-    undistortedImage2 = undistortedImage1[y:y+h, x:x+w]
-    
-    # save images
-    img_name = os.path.split(fname)
-    cv.imwrite('02_undis_img/'+ img_name[-1], undistortedImage1)
-    cv.imwrite('03_undis_img_roi/'+ img_name[-1], undistortedImage2)
-    
-    # # show results
-    # cv.imshow('undistortedImage1', undistortedImage1)
-    # cv.imshow('undistortedImage2', undistortedImage2)
-    # cv.waitKey()
-    
-    # display the result
-    print(fname + ': undistorted and cropped')
+    # read images
+    images = glob.glob(os.path.join(input_path, prefix))
+    for fname in images: 
+        targetImage = cv.imread(fname)
+        
+        # camera array
+        targetImageSize = targetImage.shape[:2][::-1]
+        alpha = 1  # 1:black
+        newKK, roiSize = cv.getOptimalNewCameraMatrix(KK, distCoeffs, targetImageSize, alpha, targetImageSize)
+        
+        # distotion correct map
+        mapX, mapY = cv.initUndistortRectifyMap(KK, distCoeffs, None, newKK, targetImageSize, cv.CV_32FC1)
+        
+        # distortion
+        undistortedImage1 = cv.remap(targetImage, mapX, mapY, cv.INTER_LINEAR)
+        
+        # remove black parts
+        x, y, w, h = roiSize
+        undistortedImage2 = undistortedImage1[y:y+h, x:x+w]
+        
+        # save images
+        img_type = os.path.split(fname)
+        img_name1 = 'undistort' + img_type[-1]
+        img_name2 = 'undistort_remove' + img_type[-1]
+        if output_path is not None:
+            output_dir = output_path
+        else:
+            output_dir = input_path
 
-cv.destroyAllWindows()
+        cv.imwrite(os.path.join(output_dir, img_name1), undistortedImage1)
+        cv.imwrite(os.path.join(output_dir, img_name2), undistortedImage2)
+        
+        # # show results
+        # cv.imshow('undistortedImage1', undistortedImage1)
+        # cv.imshow('undistortedImage2', undistortedImage2)
+        # cv.waitKey()
+        
+        # display the result
+        print(fname + ': undistorted and cropped')
+
+correct_distortion('/home/paul/Workspaces/python/Drone_io_pipeline-main/Thermal/RGB_IR_overlay/thermal_analysis/calibration_rgb_raw/', True, 
+                    '/home/paul/Workspaces/python/Drone_io_pipeline-main/Thermal/RGB_IR_overlay/thermal_analysis/calibration_rgb_undistort/')
